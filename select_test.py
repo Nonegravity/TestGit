f41278360bb7be76a68f5f7df1f3d57413457c44
Yeounggeun Kim <gworld399@naver.com>
2022-09-04 21:38:08 +0900

    First COMMIT

---
+++ select_test.py
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, inital-scale-1.0">
+    <title>Documet</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1> 만들다 프로젝트 내 손으로</h1>
+    <p> 힘들다 소스코드 만드는것도</p>
+    <h2>
+        정신차려 각박한 세상속에서
+    </h2>
+</body>
+</html>
